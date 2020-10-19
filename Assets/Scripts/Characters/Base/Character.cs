0cdfe04e242022612d5dae351a6622ea3c272c62
kokojae <54296776+kokojae@users.noreply.github.com>
2020-10-19 09:44:29 +0900

    대규모 수정
    프로젝트를 3D기반에서 2D기반으로 재생성,
    테스트 리소스 추가,
    정리용 폴더 추가 생성

---
+++ Assets/Scripts/Characters/Base/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private int characterLevel;
+    private string characterName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
